674b5a9ad4c66bc7c90c14ac91e9ef51fa2ae299
Janis <janis.gailis@uia.no>
2018-11-02 18:42:12 +0100

    Lagt inn funksjon lin_pendel_euler

--- README.md
+++ README.md
@@ -42,16 +42,20 @@ slopev_array = []
 slopetheta_array = []
 ```
 
-Det er vanlig å bygge en tabell som inneholder en indeks for hvert steg k, og tilsvarende verdier som definerer diskrete punkter i planet, - t(k), v(k), theta(k),
+Det er vanlig å bygge en tabell som inneholder en indeks for hvert steg k, og tilsvarende verdier som definerer diskrete punkter i planet, - t(k), theta(k), v(k), slopev(k), slopetheta(k):
 
 k  | t(k)  | theta(k) | v(k)   | slopev(k)| slopetheta(k)
 ---| ------|----------|--------|----------|--------------
 0  | 0     | 1.571	  | 0	     | -15.412  | 0.0
 1	 | 0.125 | 1.571	  | -1.927 | -15.412  | -1.927
 2	 | 0.25	 | 1.33	    | -3.854 | -13.047  | -3.854
+ | | | | |
 
 
 I filen [euler_pendelum_v2](oblig2/euler_pendelum_v2.py) er en løkke laget og en plot-funksjon implementert.
+Python lister er brukt slik at de er definert tomme og verdiene er lagt inn fortløpende med `appends`.
+Innebygde funksjon `range` (https://docs.python.org/3/library/stdtypes.html#range) brukes for å itererer over verdiene i Euler ligningene.
+
 
 Et eksempel på en numerisk løsningsforslag av det lineære systemet (3):
 ![oblig2 d) numerisk løsning](/images/linear_euler_v2.png)
@@ -60,4 +64,16 @@ I filen [euler_pendelum_v3](oblig2/euler_pendelum_v3.py)
 ```python
 v, theta = lin_pendel_euler(v0, theta0, g, L, N, h);
 ```
-skal implementeres.
+er implementert.
+
+Koden kan optimaliseres videre, blant annet listen med tidsverdier burde bli tatt ut av funksjonen `lin_pendel_euler`.
+
+Funksjoner brukes på følgende møte:
+```python
+# Using the Euler's method to approximate a solution of the linearized
+# version of differential equations
+v, theta = lin_pendel_euler(v0, theta0, g, L, N, h)
+
+# plott resultatene
+plot_results(t, theta)
+```
--- oblig2/euler_pendelum_v3.py
+++ oblig2/euler_pendelum_v3.py
@@ -1,7 +1,7 @@
 import math
 from matplotlib import pyplot as plt
 
-# Funksjon for å vise resultater
+# Function to plot the plot_results
 def plot_results(time, theta1):
     plt.plot(time, theta1)
     s = '(Initelle vinkelen = ' + str(theta0) + ' degrees)'
@@ -10,21 +10,12 @@ def plot_results(time, theta1):
     plt.legend(['ikke linear'], loc='lower right')
     plt.show()
 
-# Pseudo-code
+# Algorithm description
 # Define constants and variables (temporary storage)
 # Define initial conditions (time, theta and v)
-# Iterate over a sequence of integers (for each k do the following)
-#  # Values at the next step (k=2)
-# t2 = t1 + h
-# theta2 = theta1 + h*slopetheta_array[1]
-# v2 = v1 + h*slopev_array[1]
+# Call the function v, theta = lin_pendel_euler(v0, theta0, g, L, N, h)
+# Inside function iterate and use Euler's equations
 
-# Definition of storage; store the values for ploting and calculations
-t = []
-theta = []
-v = []
-slopev_array = []
-slopetheta_array = []
 
 # Constants ++
 # Will be parameters to the function
@@ -32,38 +23,54 @@ slopetheta_array = []
 g = 9.81
 L = 1.0
 T = 4.0
-#N = math.pow(2,5)
 N = 2**5
-print(N)
 h = T/N
-p = 8
+
+# building array with time values to be used in plot
+# not good if function goes in its own package
+# In the pendelum.py this is done by np.arange(0, 10, 0.025)
+# but that requires the package numpy
+t = []
+t0 = 0
 
 # Initial values
-t0 = 0 # Using this to follow the theory for the Euler's method
-theta0 = round(math.pi/2, p) # Can also used built-in pow() or ** math.pow() converts arguments to float first
+theta0 = math.pi/2 # Can also used built-in pow() or ** math.pow() converts arguments to float first
 v0 = 0
-t.append(t0)
-theta.append(theta0)
-v.append(v0)
 
-# And initial calculations
-slopev_array.append(round(-g*theta[0], p))
-slopetheta_array.append(round(v[0]/L, p))
+def lin_pendel_euler(v0, theta0, g, L, N, h):
+    # Definition of storage; store the values for ploting and calculations
+    theta = []
+    v = []
+    slopev_array = []
+    slopetheta_array = []
+
+    # Store the initial values in Python lists
+    t.append(t0)
+    theta.append(theta0)
+    v.append(v0)
+    # And initial calculations
+    slopev_array.append(-g*theta[0])
+    slopetheta_array.append(v[0]/L)
+
+    # Use built-in range to iterate over a sequence of numbers k = 1, 2, ... N-1
+    for k in range(1, N):
+        # Values at the next step k
+        tt = t[k-1] + h
+        thetat = theta[k-1] + h*slopetheta_array[k-1]
+        vt = v[k-1] + h*slopev_array[k-1]
+
+        # Store the values in the Python lists for the point k+1 (because starting at 0)
+        t.append(tt)
+        theta.append(thetat)
+        v.append(vt)
+        slopev_array.append(-g*theta[k])
+        slopetheta_array.append(v[k]/L)
+    return v, theta
 
 
-# Use built-in range to iterate over a sequence of numbers k = 1, 2, ... N-1
-for k in range(1, N):
-# Values at the next step k
-    tt = t[k-1] + h
-    thetat = theta[k-1] + h*slopetheta_array[k-1]
-    vt = v[k-1] + h*slopev_array[k-1]
-
-    # Store the values in the Python lists for the point k+1 (because starting at 0)
-    t.append(tt)
-    theta.append(thetat)
-    v.append(vt)
-    slopev_array.append(round(-g*theta[k], p))
-    slopetheta_array.append(round(v[k]/L, p))
+# Using the Euler's method to approximate a solution of the linearized
+# version of differential equations
+v, theta = lin_pendel_euler(v0, theta0, g, L, N, h)
 
 # plott resultatene
 plot_results(t, theta)
